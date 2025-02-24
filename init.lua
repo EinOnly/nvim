8e50e5418e518477fa73871a4e055bbeb8761c09
Einoly <citylosttan@gmail.com>
2025-02-24 12:54:36 +0800

    Auto find and set python env...

--- init.lua
+++ init.lua
@@ -15,3 +15,24 @@ vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 vim.wo.foldlevel = 0
 vim.cmd([[highlight Folded guibg=NONE guifg=fg]])
+
+-- Auto activate python env
+local env_dirs = { ".venv", "env", "venv" }
+local found_env = false
+local cur_env = ""
+
+for _, dir in ipairs(env_dirs) do
+	local env_path = vim.fn.getcwd() .. "/" .. dir .. "/bin/python"
+	if vim.fn.executable(env_path) == 1 then
+		vim.g.python3_host_prog = env_path
+		found_env = true
+		cur_env = env_path
+		break
+	end
+end
+
+if not found_env then
+	print("No virtual environment found (checked .venv, env, venv)")
+else
+	print("Current python env:", cur_env)
+end
